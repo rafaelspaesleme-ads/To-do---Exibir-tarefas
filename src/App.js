543ee26ffbf322b479dc0037bd103312d093a660
samuelrrs <samuellrrs@gmail.com>
2020-10-27 16:16:28 -0300

    Primeiro commit

--- src/App.js
+++ src/App.js
@@ -1,25 +1,68 @@
-import logo from './logo.svg';
-import './App.css';
+import React, { useState } from "react";
+import useForm from "./hooks/useForm";
 
-function App() {
+import "./app.css";
+export default function App() {
+  const [tasks, setTasks] = useState([
+    /* { id: 1, name: "tarefa 1", description: "arrumar o erro 1" },
+    { id: 2, name: "tarefa 2", description: "arrumar o erro 2" },
+    { id: 3, name: "tarefa 3", description: "arrumar o erro 3" }, */
+  ]);
+  const [{ values, loading }, handleChange, handleSubmit] = useForm();
+
+  const enviarContato = () => {
+    // faça o que for preciso :)
+    console.log(values);
+  };
+
+  function handleAddTasks() {
+    setTasks((prevState) => [
+      ...prevState,
+      {
+        id: Math.random(),
+        name: "Nova Tarefa",
+        description: "arrumar erro",
+      },
+    ]);
+  }
+
   return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
-    </div>
+    <>
+      <div id="principal">
+        <div id="container">
+        <form onSubmit={handleSubmit(enviarContato)}>
+        <input
+          onChange={handleChange}
+          type="text"
+          name="title"
+          placeholder="Tarefa"
+        />
+        <input
+          onChange={handleChange}
+          type="text"
+          name="desc"
+          placeholder="Descricao "
+        />
+        <input
+          onChange={handleChange}
+          type="text"
+          name="data"
+          placeholder="Data"
+        />
+        <button type="submit">{loading ? "Enviando..." : "Enviar"}</button>
+      </form>
+        
+        
+        <ul>
+          {tasks.map((task) => (
+            <li key={task.id}>
+              {task.name}, {task.description}
+            </li>
+          ))}
+        </ul>
+        </div>
+      
+      </div>
+    </>
   );
 }
-
-export default App;
