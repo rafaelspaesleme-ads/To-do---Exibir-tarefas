4c5a5bb49b7c0b6797308fa5a98dbb50fa9774d5
samuelrrs <samuellrrs@gmail.com>
2020-11-12 10:02:46 -0300

    fix button

--- src/components/ButtonDelete/index.jsx
+++ src/components/ButtonDelete/index.jsx
@@ -3,7 +3,7 @@ import IconButton from "@material-ui/core/IconButton";
 import DeleteIcon from "@material-ui/icons/Delete";
 
 export const ButtonDelete = ({onClickDelete}) => {
-
+ 
     return (
         <IconButton
             aria-label="Delete"
